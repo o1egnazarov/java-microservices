9175cf297984de9ff12b1474d6363b07ba360046
o1egnazarov <noleg867@gmail.com>
2025-07-04 13:59:15 +0600

    init currency-rate service

---
+++ currency-rate/src/test/java/ru/noleg/currencyrate/CurrencyRateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.noleg.currencyrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
